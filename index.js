8a58099f253850effd64ff3ce1a9083cafee1eba
VincentKempers <vincent.m.kempers@gmail.com>
2017-12-29 02:10:59 +0100

    need to get some sleep

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 var name = 'vincent';
 
-console.log(name);
+var meerdere = document.getElementById('meerdere');
+var meerdereText = document.getElementById('meerdere-tekst');
+var startButton = document.getElementById('start');
+
+var alleen = document.getElementById('alleen');
+var alleenText = document.getElementById('alleen-tekst');
+
+function startGameMeerdere() {
+	alleenText.style.display = 'none';
+	meerdereText.style.display = 'block';
+	startButton.removeAttribute('disabled');
+}
+
+function startGameAlleen() {
+	meerdereText.style.display = 'none';
+	alleenText.style.display = 'block';
+	startButton.removeAttribute('disabled');
+}
+
+meerdere.addEventListener('click', startGameMeerdere);
+alleen.addEventListener('click', startGameAlleen);
+
