21a929fe6ca1fc86dfa7e9e55d4321e16dd9fb0f
VincentKempers <vincent.m.kempers@gmail.com>
2018-01-03 23:44:50 +0100

    dont want to lose this

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-var nameOne;
+let nameOne;
 
-var meerdere = document.getElementById('meerdere');
-var meerdereText = document.getElementById('meerdere-tekst');
-var startButton = document.getElementById('start');
+const meerdere = document.getElementById('meerdere');
+const meerdereText = document.getElementById('meerdere-tekst');
+const startButton = document.getElementById('start');
+const alleen = document.getElementById('alleen');
+const alleenText = document.getElementById('alleen-tekst');
 
-var alleen = document.getElementById('alleen');
-var alleenText = document.getElementById('alleen-tekst');
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+let score = 0;
 
 function startGameMeerdere() {
 	alleenText.style.display = 'none';
 	meerdereText.style.display = 'block';
 	startButton.removeAttribute('disabled');
+	// writeCookie();
 }
 
 function startGameAlleen() {
@@ -20,9 +26,47 @@ function startGameAlleen() {
 }
 
 function storeName() {
-	nameOne = prompt('Vul hier je naam in', 'jouw naam');
+	nameOne = prompt('vul hier je naam in', 'Jouw Naam');
+	localStorage.setItem('name', nameOne);
 }
+
+//  function WriteCookie()
+//             {
+//                if( document.myform.contestant.value == "" ){
+//                   alert("Enter some value!");
+//                   return;
+//                }
+//                cookievalue= escape(document.myform.contestant.value) + ";";
+//                document.cookie="name=" + cookievalue;
+//             }
+
+
+// function ReadCookie()
+//             {
+//                let allcookies = document.cookie;
+//                document.write ("All Cookies : " + allcookies );
+               
+//                // Get all the cookies pairs in an array
+//                cookiearray = allcookies.split(';');
+//                console.log(cookiearray);
+
+//                // Now take key value pair out of this array
+//                for(let i=0; i<cookiearray.length; i++){
+//                   name = cookiearray[i].split('=')[0];
+//                   value = cookiearray[i].split('=')[1];
+//                   nameOne = value;
+//                   document.write ("Key is : " + name + " and Value is : " + value);
+//                }
+//             }
+
+
+
 
 meerdere.addEventListener('click', startGameMeerdere);
 alleen.addEventListener('click', startGameAlleen);
-startButton.addEventListener('click', storeName, true)
+startButton.addEventListener('click', storeName, true);
+
+
+
+console.log(localStorage.getItem('name'));
+
